aa57f2f8f40d45631fd5f483ce6ccab9a5d0e0f7
andresf2448 <andresf2448@gmail.com>
2023-09-20 19:58:19 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,58 @@
+// comentario de linea
+
+/* 
+comentario 
+de 
+bloque
+*/
+
+//forma recomendada para declarar variables
+// let edad = 5; // una variable de tipo number
+// let nombre = "andres"; // una variable de tipo string
+
+// //forma no recomendada para declarar variables
+// var direccion = "AV";
+
+// const IVA = 0.21;
+
+// nombre = "juan";
+
+// console.log(direccion);
+// console.log("Hola a todos", direccion);
+
+// camelCase => nombreBonitoGrande
+// PascalCase => NombreBonitoGrande
+// snake_case => nombre_bonito_grande
+
+// let numeroUno = 5;
+// let numeroDos = 10;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("Resultado suma", resultadoSuma);
+// console.log("Resultado resta", resultadoResta);
+// console.log("Resultado multiplicacion", resultadoMultiplicacion);
+// console.log("Resultado division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = "pepito";
+
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+// console.log(saludo);
+// console.log(nombre);
+
+// let nombre = prompt("Ingrese su nombre");
+// alert("Hola " + nombre);
+
+// let numero = 5;
+// let nombre = "pepito";
+
+// console.log(typeof numero);
+// console.log(typeof nombre);
+
+let numero = Number(prompt("Ingrese un numero"));
+alert(numero + 4)
